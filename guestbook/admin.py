adcb832846224d0284985aee6b0567c83a63b522
Almukhan Chingiz <chingizpain@gmail.com>
2022-10-01 11:33:14 +0600

    Создал модель гостевой книги и зарегистрировал ее в админке

--- guestbook/admin.py
+++ guestbook/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
 
+from guestbook.models import GuestBook
+
 # Register your models here.
+admin.site.register(GuestBook)
--- guestbook/models.py
+++ guestbook/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+STATUSES = (('active', 'Активно'), ('blocked', 'Заблокировано'))
+
+
+class GuestBook(models.Model):
+    author = models.CharField(verbose_name='Автор', max_length=200, null=False, blank=False)
+    email = models.EmailField(verbose_name='Почта', max_length=254, null=False, blank=False)
+    text = models.TextField(verbose_name='Текст', max_length=200, null=False, blank=False)
+    created_at = models.DateTimeField(verbose_name='Дата добавления', auto_now_add=True)
+    changed_at = models.DateTimeField(verbose_name='Дата изменения', auto_now=True)
+    status = models.CharField(verbose_name='Статус', max_length=20, null=False, blank=False, choices=STATUSES,
+                              default='active')
+
+    def __str__(self):
+        return f'{self.author} - {self.status}'
