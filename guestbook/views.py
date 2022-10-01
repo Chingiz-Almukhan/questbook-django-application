c9ab05e8714a969bcc0b9d056dc50cbe913c3950
Almukhan Chingiz <chingizpain@gmail.com>
2022-10-01 14:58:18 +0600

    Изменил метод отображения главной страницы, добавил валидацию к форме добавления Добавил метод для редактирования и удаления

--- guestbook/views.py
+++ guestbook/views.py
@@ -1,12 +1,52 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
+from guestbook.forms import AddEditForm
 from guestbook.models import GuestBook
 
 
 # Create your views here.
 def index_view(request):
+    form = AddEditForm()
     guestbook = GuestBook.objects.filter(status='active').order_by('-created_at')
     context = {
-        'guestbook': guestbook
+        'guestbook': guestbook,
+        'form': form
     }
+    if request.method == 'POST':
+        form = AddEditForm(data=request.POST)
+        if form.is_valid():
+            create = GuestBook.objects.create(author=form.cleaned_data['author'],
+                                              email=form.cleaned_data["email"], text=form.cleaned_data['text'])
+            return redirect('main')
+        else:
+            return render(request, 'main.html', context={'form': form,
+                                                         'guestbook': guestbook})
     return render(request, 'main.html', context)
+
+
+def edit(request, pk):
+    guest = GuestBook.objects.get(pk=pk)
+    if request.method == 'GET':
+        form = AddEditForm(initial={
+            'author': guest.author,
+            'email': guest.email,
+            'text': guest.text
+
+        })
+        return render(request, 'edit.html', context={'form': form, 'guest': guest})
+    elif request.method == 'POST':
+        form = AddEditForm(data=request.POST)
+        if form.is_valid():
+            guest.author = form.cleaned_data['author']
+            guest.email = form.cleaned_data["email"]
+            guest.text = form.cleaned_data['text']
+            guest.save()
+            return redirect('main')
+        else:
+            return render(request, 'edit.html', context={'form': form, 'guest': guest})
+
+
+def delete(request, pk):
+    guest = GuestBook.objects.get(pk=pk)
+    guest.delete()
+    return redirect('main')
