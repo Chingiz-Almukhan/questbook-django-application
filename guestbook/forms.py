6545378e97a7cfd99d2cb2aba167fc532ac7c6da
Almukhan Chingiz <chingizpain@gmail.com>
2022-10-01 17:20:59 +0600

    Изменил форму добавления и редактирования Также добавил форму для поиска

--- guestbook/forms.py
+++ guestbook/forms.py
@@ -6,10 +6,17 @@ class AddEditForm(forms.Form):
                              widget=forms.TextInput(
                                  attrs={'placeholder': 'Введите автора',
                                         'type': 'text', 'class': 'form-control mb-2'}))
-    email = forms.CharField(max_length=254, required=True, label='Email',
-                            widget=forms.TextInput(
-                                attrs={'placeholder': 'Введите email',
-                                       'type': 'text', 'class': 'form-control mb-2'}))
+    email = forms.EmailField(max_length=254, required=True, label='Email',
+                             widget=forms.TextInput(
+                                 attrs={'placeholder': 'Введите email',
+                                        'type': 'text', 'class': 'form-control mb-2'}))
     text = forms.CharField(max_length=200, required=True, label='Text',
                            widget=forms.Textarea(
                                attrs={'placeholder': 'Введите текст', 'rows': '3', 'class': 'form-control mb-2'}))
+
+
+class SearchForm(forms.Form):
+    search = forms.CharField(max_length=200, required=True, label='Author',
+                             widget=forms.TextInput(
+                                 attrs={'placeholder': 'Введите автора',
+                                        'type': 'text', 'class': 'form-control mb-2'}))
