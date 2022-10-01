5b7ecfa24728cc4deee19df3179f862028f12156
Almukhan Chingiz <chingizpain@gmail.com>
2022-10-01 12:34:25 +0600

    Была создана вьюха, которая передавала отсортированные записи

--- guestbook/views.py
+++ guestbook/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
 
+from guestbook.models import GuestBook
+
+
 # Create your views here.
+def index_view(request):
+    guestbook = GuestBook.objects.filter(status='active').order_by('-created_at')
+    context = {
+        'guestbook': guestbook
+    }
+    return render(request, 'main.html', context)
