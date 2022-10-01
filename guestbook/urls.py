072922893f2358460c7f683548345b6f0397b687
Almukhan Chingiz <chingizpain@gmail.com>
2022-10-01 14:57:28 +0600

    В url прописал пути к методам редактирования и удаления

--- guestbook/urls.py
+++ guestbook/urls.py
@@ -1,16 +1,10 @@
 from django.urls import path
 
-from guestbook.views import index_view
+from guestbook.views import index_view, edit, delete
 
 urlpatterns = [
     path('', index_view, name='main'),
-    # path('products/<int:pk>', product_view, name='product'),
-    # path('products/add', add_view, name='add'),
-    # path('categories/add', category_view, name='category_add'),
-    # path('products/<int:pk>/edit', edit_view, name='edit'),
-    # path('products/<int:pk>/delete', delete_view, name='delete'),
-    # path('categories/', categories_view, name='categories'),
-    # path('categories/<int:pk>/edit', category_edit_view, name='category_edit'),
-    # path('categories/<int:pk>/delete', category_delete_view, name='category_delete'),
+    path('edit/<int:pk>/', edit, name='edit'),
+    path('delete/<int:pk>/', delete, name='delete'),
 
 ]
