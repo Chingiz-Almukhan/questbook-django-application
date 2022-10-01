f2cb3df69e04869b50c360a440654a39fd50f3b9
Almukhan Chingiz <chingizpain@gmail.com>
2022-10-01 17:22:31 +0600

    Создал методы поиска и добавления Также указал их в url

--- guestbook/urls.py
+++ guestbook/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
 
-from guestbook.views import index_view, edit, delete
+from guestbook.views import index_view, edit, delete, add, search_view
 
 urlpatterns = [
     path('', index_view, name='main'),
+    path('add/', add, name='add'),
     path('edit/<int:pk>/', edit, name='edit'),
     path('delete/<int:pk>/', delete, name='delete'),
+    path('search/', search_view, name='search')
 
 ]
--- guestbook/views.py
+++ guestbook/views.py
@@ -1,26 +1,19 @@
 from django.shortcuts import render, redirect
 
-from guestbook.forms import AddEditForm
+from guestbook.forms import AddEditForm, SearchForm
 from guestbook.models import GuestBook
 
 
 # Create your views here.
 def index_view(request):
     form = AddEditForm()
+    search_form = SearchForm()
     guestbook = GuestBook.objects.filter(status='active').order_by('-created_at')
     context = {
         'guestbook': guestbook,
-        'form': form
+        'form': form,
+        'search_form': search_form
     }
-    if request.method == 'POST':
-        form = AddEditForm(data=request.POST)
-        if form.is_valid():
-            create = GuestBook.objects.create(author=form.cleaned_data['author'],
-                                              email=form.cleaned_data["email"], text=form.cleaned_data['text'])
-            return redirect('main')
-        else:
-            return render(request, 'main.html', context={'form': form,
-                                                         'guestbook': guestbook})
     return render(request, 'main.html', context)
 
 
@@ -50,3 +43,35 @@ def delete(request, pk):
     guest = GuestBook.objects.get(pk=pk)
     guest.delete()
     return redirect('main')
+
+
+def add(request):
+    guestbook = GuestBook.objects.filter(status='active').order_by('-created_at')
+    form = AddEditForm(data=request.POST)
+    search_form = SearchForm()
+    if form.is_valid():
+        create = GuestBook.objects.create(author=form.cleaned_data['author'],
+                                          email=form.cleaned_data["email"], text=form.cleaned_data['text'])
+        return redirect('main')
+    else:
+        return render(request, 'main.html', context={'form': form,
+                                                     'guestbook': guestbook,
+                                                     'search_form': search_form})
+
+
+def search_view(request):
+    guestbook = GuestBook.objects.filter(status='active').order_by('-created_at')
+    form = SearchForm(data=request.POST)
+    add_form = AddEditForm()
+    if form.is_valid():
+        search_result = request.POST.get('search')
+        guestbook = GuestBook.objects.filter(author=search_result).order_by('-created_at')
+        search_form = SearchForm()
+        context = {
+            'guestbook': guestbook,
+            'form': add_form,
+            'search_form': search_form
+        }
+        return render(request, 'main.html', context)
+    else:
+        return render(request, 'main.html', context={'form': add_form, 'guest': guestbook, 'search_form': form})
